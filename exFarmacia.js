fba91c65fea55edacfdb38404d852cb2089763ab
João Pucci <126732888+taldoJonas1@users.noreply.github.com>
2023-08-29 20:27:32 -0300

    Update exFarmacia.js

--- exFarmacia.js
+++ exFarmacia.js
@@ -3,12 +3,12 @@ function exFarmacia(){
     let remedio = []
 
     cadastroFarmacias(farmacia);
-    cadastroRemedios(remedio);
+    cadastroRemedios(remedio, farmacia);
+    vendaRemedios(remedio)
 
-
-    function cadastroFarmacias(vet){
+    function cadastroFarmacias(farmacia){
         for(let i=0; i<5; i++){
-            let obj = {
+            let objF = {
                 codigoF: Number(prompt(`Insira o código da farmácia ${i+1}`)),
                 nomeF: prompt(`Informe o nome da farmácia ${i+1}`).toUpperCase(),
                 enderecoF: prompt(`Informe o endereço da farmacia ${i+1}`).toUpperCase()
@@ -16,17 +16,48 @@ function exFarmacia(){
             while(farmacia.some((event) => event.codigo == farmacia.codigo)){
                 farmacia.codigo = Number(prompt(`Insira o código da farmácia ${i+1}`))
             }
-            vet.push(obj)
+            farmacia.push(objF)
         }
     }
 
-    function cadastroRemedios(rem){
+    function cadastroRemedios(remedio, farmacia){
         for(let i=0; i<5; i++){
-            let obj2 = {
+            let objR = {
                 codR: Number(prompt(`Insira o código do remédio ${i+1}`)),
                 nomeR: Number(prompt(`Insira o nome do remédio ${i+1}`)).toUpperCase(),
-                qtdF:,
-                precoR:
+                qtdR: Number(prompt(`Informe a quantidade em estoque do remédio ${i+1}`)),
+                precoR: Number(prompt(`Informe o preço do remédio ${i+1}`))
+            }
+            while(!farmacia.some((farm) => farm.codigoF == remedio.codigoF)){
+                remedio.codigoF = prompt(`Código da farmácia inválido, insira outro.`)
+            }
+            remedio.push(objR)
+        }
+    }
+
+    function vendaRemedios(rem){
+        for(let i=0; i<5; i++){
+            let objV = {
+                codigoV: Number(prompt("Informe o código da farmácia: ")),
+                vendaR: prompt("Informe o remédio a ser vendido: "),
+                qtdV: Number(prompt("Informe a quantidade desejada: "))
+            }
+
+            let achou = false
+
+            for(i=0; i<rem.length; i++){
+                if(rem[i].codigoF == objV.codigoV && rem[i].nomeR == objR.vendaR){
+                    achou = true
+                    if(rem[i].qtdR >= objV.qtdV){
+                        rem[i].qtdR = rem[i].qtdR - objV.qtdV
+                    }
+                    else{
+                        alert("Quantidade em estoque insuficiente.")
+                    }
+                }
+            }
+            if(!achou){
+                alert("Produto não encontrado")
             }
         }
     }
