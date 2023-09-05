eaf1f4c07cce4580501779ccec51e4df01dcbe0e
João Pucci <126732888+taldoJonas1@users.noreply.github.com>
2023-09-04 22:13:13 -0300

    Update exPoliticos.js

--- exPoliticos.js
+++ exPoliticos.js
@@ -1,5 +1,21 @@
-exPoliticos()
-
 let exPoliticos = () => {
+    let vet = []
+    let opcao
 
+    do{
+        opcao = Number(prompt('Escolha \n1. Criar novo partido \n2. Adicionar político \n3. Iniciar votação\n4. Exibir mais votado\n5. Sair'))
+        switch(opcao){
+        case 1:
+            break
+        case 2:
+            break
+        case 3:
+            break
+        case 4:
+            break
+        case 5:
+            break
+        default:
+        }
+    }while(opcao != 5)
 }
