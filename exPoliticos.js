640bdceb91a7b9dfd4c1731b7157f6738e61cff3
João Pucci <126732888+taldoJonas1@users.noreply.github.com>
2023-09-05 20:40:34 -0300

    Update exPoliticos.js

--- exPoliticos.js
+++ exPoliticos.js
@@ -5,17 +5,17 @@ let exPoliticos = () => {
     do{
         opcao = Number(prompt('Escolha \n1. Criar novo partido \n2. Adicionar político \n3. Iniciar votação\n4. Exibir mais votado\n5. Sair'))
         switch(opcao){
-        case 1:
+        case 1: novoPartido()
             break
-        case 2:
+        case 2: addPolitico()
             break
-        case 3:
+        case 3: iniciarVotacao()
             break
-        case 4:
+        case 4: resultadoVotacao()
             break
-        case 5:
+        case 5: alert('O programa foi encerrado.')
             break
-        default:
+        default: alert('Escolha uma das opções de 1 a 5')
         }
     }while(opcao != 5)
 }
